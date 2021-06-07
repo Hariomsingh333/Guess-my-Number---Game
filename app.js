c5488223a55775882a32275c6e1d3bece3399f8a
Hari Om Singh <www.hariom03.com@gmail.com>
2021-06-07 20:46:04 +0530

    add condition statement with msg

--- app.js
+++ app.js
@@ -5,10 +5,27 @@ const body = document.querySelector("body");
 darkBtn.addEventListener("click", () => {
   body.classList.toggle("dark");
 });
+// secret number
+const secretNumber = Math.floor(Math.random() * 20 + 1);
+const questionMark = document.querySelector(".question-mark");
+questionMark.textContent = secretNumber;
+console.log(secretNumber);
 
 // check button
-const guessNumber = document.querySelector(".guess-number");
+
 const checkBtn = document.querySelector(".check");
+const msg = document.querySelector(".message");
+
 checkBtn.addEventListener("click", () => {
-  console.log(guessNumber.value);
+  const guess = Number(document.querySelector(".guess-number").value);
+  // console.log(guess);
+  if (!guess) {
+    msg.innerText = `you don't got it: 😢`;
+  } else if (guess === secretNumber) {
+    msg.innerText = `you got it: ${secretNumber}`;
+  } else if (guess >= secretNumber) {
+    msg.innerText = `too high!: 📈`;
+  } else if (guess <= secretNumber) {
+    msg.innerText = `too low!: 📉`;
+  }
 });
---
+++ note.md
@@ -0,0 +1,11 @@
+i just try to make a game and there it is.
+
+<!-- what i use to make the game -->
+
+i made this game with very simply things like: Html, css and javascript.
+
+<!-- paling  -->
+
+- basic html h1 and p and input
+- basic css and gaming font
+- js is the main things to thinking in deep-dive
